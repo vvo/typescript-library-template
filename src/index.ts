17adbafa6a0679961d9e3b91c63ff6a00795a159
Vincent Voyer <vincent@codeagain.com>
2020-07-28 14:07:40 +0200

    chore(lint): fix lint

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export const sum = (a: number, b: number) => {
+export const sum = (a: number, b: number): number => {
   if ("development" === process.env.NODE_ENV) {
     console.log("boop");
   }
